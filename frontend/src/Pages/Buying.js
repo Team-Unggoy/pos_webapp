32ba7dfe093332143b55f6ebf44dbb3a8127684c
BevanTony <babantoto@gmail.com>
2021-03-03 17:00:01 +0800

    need to fix total summary

--- frontend/src/Pages/Buying.js
+++ frontend/src/Pages/Buying.js
@@ -4,11 +4,24 @@ import Datepicker from '../Components/Datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Table } from 'semantic-ui-react'
-import { TextField, Checkbox } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 import { Button } from 'semantic-ui-react'
 import DeleteIcon from '@material-ui/icons/Delete';
 import { Search } from 'semantic-ui-react'
+import { withStyles } from '@material-ui/styles';
 import _ from 'lodash'
+
+const styles = theme => ({
+    root: {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 3,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+    },
+  });
 
 
 class Buying extends React.Component{
@@ -21,10 +34,12 @@ class Buying extends React.Component{
             results:[],
             buyingList:[],
             value:'',
-            checked:false,
-            
-
+            total:0,
+            grandTotal:0,
+            total_qty:0,
+            status:false,
         }
+        
         this.fetchItem = this.fetchItem.bind(this)
         this.handleSearchChange = this.handleSearchChange.bind(this)
 
@@ -105,27 +120,34 @@ class Buying extends React.Component{
                 (item) => item.key === result.key
               );
             if(itemIndex !== -1){
+                console.log('ga dagan man ko diri diba?')
                 this.setState(prevState => ({
                     value:'',
                     buyingList:prevState.buyingList.map(
                         
                         el => el.key === result.key? { value:'', ...el, qty:el.qty +1, total:(el.qty+ 1) * el.cost }: el
-                    )
+                    ),
+                    total:parseFloat(prevState.total) + parseFloat(result.price),
+                    
+                    
                     
                 }))
             }
             else{
-            const obj = {'key':result.key, 'name': result.title, 'cost':result.price, 'qty':1, 'total':result.price * 1, isChecked:false}
-            this.setState({
+                console.log(result.total)
+            const obj = {'key':result.key, 'name': result.title, 'cost':result.price, 'qty':1, 'total':result.price * 1,}
+            this.setState(prevState =>({
                 buyingList:[
                     ...this.state.buyingList,
                     obj,
+                    
 
                 ],
+                total:parseFloat(prevState.total) + parseFloat(result.price),
                 value:'',
 
                 
-            })
+            }))
             console.log(this.state.buyingList)
         }
         }
@@ -133,8 +155,9 @@ class Buying extends React.Component{
         qtyHandle = (event, row, index) => {
             this.setState(prevState => ({
                 buyingList:prevState.buyingList.map(
-                    el => el.key === row.key? { ...el, qty: parseInt(event.target.value), total: parseInt(event.target.value) * el.cost}: el
-                )
+                    el => el.key === row.key? { ...el, qty: event.target.value, total: event.target.value * parseFloat(el.cost).toFixed(2)}: el
+                ),
+                total: prevState.total + 1,
             }))
         }
 
@@ -146,11 +169,13 @@ class Buying extends React.Component{
         }
 
         deleteItem = (row,index) =>{
+            console.log(row.total)
             var list = [...this.state.buyingList]
             list.splice(index, 1);
-            this.setState({
-                buyingList:list
-            })
+            this.setState(prevState =>({
+                buyingList:list,
+                total: parseFloat(prevState.total).toFixed(2) - parseFloat(row.total).toFixed(2),
+            }))
         }
         
 
@@ -158,22 +183,23 @@ class Buying extends React.Component{
 
         
     render(){
-
+        const { classes } = this.props;
         console.log(this.state.buyingList)
 
         return(
      
             <>
             
-            <div>
-            <form className='buying-form'>
-            <div className='buying-input'>
+           
 
-            <div style={{float: 'right'}}>
-            <Button primary> Submit</Button>
-            <Button secondary> Delete</Button>  
+            <div style={{float: 'right', margin:5}}>
+            <Button type='button' primary> Submit</Button>
+            <Button type='button' secondary> Delete</Button>  
             </div>
 
+            <form className='buying-form'>
+          
+            <div className='buying-input'>
  
             <Datepicker />
 
@@ -192,13 +218,13 @@ class Buying extends React.Component{
 
             <div className='table-container'>
 
-                <Table size='large' celled fixed selectable compact inverted>
-                    <Table.Header>
+                <Table size='large' celled fixed selectable compact color={'yellow'}>
+                    <Table.Header style={{  position:'sticky'}}>
                     <Table.Row>
                         <Table.HeaderCell width={4}>Item Name</Table.HeaderCell>
                         <Table.HeaderCell width={1}>Qty</Table.HeaderCell>
-                        <Table.HeaderCell width={3}>Cost</Table.HeaderCell>
-                        <Table.HeaderCell width={3}>Total</Table.HeaderCell>
+                        <Table.HeaderCell width={2}>Cost</Table.HeaderCell>
+                        <Table.HeaderCell width={2}>Total</Table.HeaderCell>
                         <Table.HeaderCell width={1}></Table.HeaderCell>
                     </Table.Row>
                     </Table.Header>
@@ -207,7 +233,7 @@ class Buying extends React.Component{
                         {this.state.buyingList.map((row, index) =>(
                             <Table.Row key={index}>
                                 <Table.Cell>{row.name}</Table.Cell>
-                                <Table.Cell> <TextField  type='number' value={row.qty} fullWidth variant='standard' size='small' onChange={(e) => {this.qtyHandle(e, row,index)}} InputProps={{style: {color: "white"}}}/></Table.Cell>
+                                <Table.Cell> <TextField  type='number' value={row.qty} fullWidth variant='standard' size='small' onChange={(e) => {this.qtyHandle(e, row,index)}} /></Table.Cell>
                                 <Table.Cell>{row.cost}</Table.Cell>
                                 <Table.Cell>{row.total}</Table.Cell>
                                 <Table.Cell><DeleteIcon onClick={() => {this.deleteItem(row,index)}}/></Table.Cell>
@@ -221,8 +247,14 @@ class Buying extends React.Component{
             <Button type='button' style={{marginTop: 5}} onClick={this.getList}>Get all Items</Button>
             </div>
             
-            </form>
+            <div className='table-info' style={{backgroundColor:'#D6D6D6'}}>
+                <h1 style={{fontSize:30,textAlign:'left',borderRadius:10, color:'#333533',backgroundColor:'#ffd100'}}>Purchase Order Summary</h1>
+                <TextField label='TOTAL' fullWidth variant='outlined' value={this.state.total}  inputProps={{ style: { fontSize:15,color: 'black', borderColor:'white'}}}></TextField>
             </div>
+
+            </form>
+
+            
             
             </>
         )
